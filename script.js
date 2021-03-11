be1a9ffaf4c4a46196541bec22e12d11a2c31cfe
Blake DeGraw <bbdegraw@gmail.com>
2021-03-11 15:49:07 -0800

    can generate constant sine wave

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const sampleRate = 44100
+// const sampleRate = 44100
 
-const sineWaveArray = new Float32Array(sampleRate)
+// const sineWaveArray = new Float32Array(sampleRate)
 
-const hz = 440
+// const hz = 440
 
-for (i = 0; i < sineWaveArray.length; i++) {
-    sineWaveArray[i] = Math.sin(i * Math.PI * 8 / hz)
-}
+// for (i = 0; i < sineWaveArray.length; i++) {
+//     sineWaveArray[i] = Math.sin(i * Math.PI * 8 / hz)
+// }
 
-function playSound({array, sampleRate}) {
-    const audioContext = new AudioContext({sampleRate})
-    const audioBuffer = audioContext.createBuffer(1, array.length, sampleRate)
-    audioBuffer.copyToChannel(array, 0)
-    const source = audioContext.createBufferSource()
-    source.connect(audioContext.destination)
-    source.buffer = audioBuffer
-    source.start()
-}
+// function playSound({array, sampleRate}) {
+//     const audioContext = new AudioContext({sampleRate})
+//     const audioBuffer = audioContext.createBuffer(1, array.length, sampleRate)
+//     audioBuffer.copyToChannel(array, 0)
+//     const source = audioContext.createBufferSource()
+//     source.connect(audioContext.destination)
+//     source.buffer = audioBuffer
+//     source.start()
+// }
 
-function playSineWave440hz() {
-    playSound({ array: sineWaveArray, sampleRate })
-}
+// function playSineWave440hz() {
+//     playSound({ array: sineWaveArray, sampleRate })
+// }
+
+
+// ****
+
+
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+
+var audioCtx = new window.AudioContext()
+
+var oscillator = audioCtx.createOscillator()
+
+oscillator.connect(audioCtx.destination)
+
