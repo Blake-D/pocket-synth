c0889c1d98308ccb7b18e836b4dd6b548a24d377
Blake DeGraw <bbdegraw@gmail.com>
2021-03-12 09:44:09 -0800

    oscillator reconfigured as an object, starts and stops at will without page refresh

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-var audioContext = new AudioContext() 
-var oscillator = audioContext.createOscillator()
-// var filter = audioContext.createBiquadFilter()
-oscillator.connect(audioContext.destination) 
-oscillator.type = "square"
-// vv if you want to run the oscillator through a highpass filter
-// oscillator.connect(filter)
-// filter.connect(audioContext.destination)
-// filter.type = "highpass"
-// filter.frequency.setTargetAtTime(2000, audioContext.currentTime, 0)
+var play, oscillator
 
-oscillator.frequency.setValueAtTime(130.4, audioContext.currentTime)
-oscillator.detune.setValueAtTime(200, audioContext.currentTime + 1)
+var oscProp = {
+    type: "sine",
+    frequency: 500,
+    playing: false
+}
+
+var audioContext = new AudioContext()
+
+window.onload = function(){
+    play = function(){
+        if(oscProp.playing){
+            oscillator.stop()
+            oscProp.playing = false
+        } else {
+            oscillator = audioContext.createOscillator()
+            oscillator.type = oscProp.type
+            oscillator.frequency.setValueAtTime(oscProp.frequency, audioContext.currentTime)
+            oscillator.connect(audioContext.destination)
+            oscillator.start()
+            oscProp.playing = true
+        }
+    }
+}
