1fb7be4beca5c2e523cf4f189d8d1f2886ab600a
Blake DeGraw <bbdegraw@gmail.com>
2021-03-11 16:34:34 -0800

    can start and stop as many times as you want

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-var audioContext = new AudioContext();
-var oscillator = audioContext.createOscillator();
-oscillator.connect(audioContext.destination);
+var audioContext = new AudioContext() 
+var oscillator = audioContext.createOscillator()
+var filter = audioContext.createBiquadFilter()
+oscillator.connect(audioContext.destination) 
+
+// vv if you want to run the oscillator through a highpass filter
+// oscillator.connect(filter)
+// filter.connect(audioContext.destination)
+// filter.type = "highpass"
+// filter.frequency.setTargetAtTime(1000, audioContext.currentTime, 0)
+
+// let activated = false
+
+// function oscillate(){
+//     if(activated == false){
+//         var audioContext = new AudioContext() 
+//         var oscillator = audioContext.createOscillator()
+//         var filter = audioContext.createBiquadFilter()
+//         oscillator.connect(audioContext.destination)
+//         oscillator.start()
+//         activated = true
+//     } else if(activated == true){
+//         oscillator.stop()
+//         activated = false
+//     }
+// }
+
+
