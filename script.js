2db608caae7230e06aab533cc0d742b53bb49591
Blake DeGraw <bbdegraw@gmail.com>
2021-03-14 12:40:45 -0700

    oscillators' play functions have been split into two separate functions so that one of them can be turned on/off

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var play, oscillator, oscillator2, changeType
+var play, play2, oscillator, oscillator2, changeType
 
 var oscProp = {
     type: "square",
@@ -16,32 +16,41 @@ var audioContext = new AudioContext()
 
 window.onload = function(){
     play = function(){
-        if(oscProp.playing && oscProp2.playing){
+        if(oscProp.playing){
             oscillator.stop()
-            oscillator2.stop()
             oscProp.playing = false
-            oscProp2.playing = false
         } else {
             oscillator = audioContext.createOscillator()
-            oscillator2 = audioContext.createOscillator()
             oscillator.type = oscProp.type
-            oscillator2.type = oscProp2.type
             oscillator.frequency.setValueAtTime(oscProp.frequency, audioContext.currentTime)
-            oscillator2.frequency.setValueAtTime(oscProp2.frequency, audioContext.currentTime)
             oscillator.connect(audioContext.destination)
-            oscillator2.connect(audioContext.destination)
             oscillator.start()
-            oscillator2.start()
             oscProp.playing = true
+        }
+    }
+
+    play2 = function(){
+        if(oscProp2.playing){
+            oscillator2.stop(
+            oscProp2.playing = false
+            )
+        } else {
+            oscillator2 = audioContext.createOscillator()
+            oscillator2.type = oscProp2.type
+            oscillator2.frequency.setValueAtTime(oscProp2.frequency, audioContext.currentTime)
+            oscillator2.connect(audioContext.destination)
+            oscillator2.start()
             oscProp2.playing = true
         }
     }
 
     changeType = function(){
         oscProp.type = document.querySelector("input[name = 'waveform']:checked").value
+        play()
+        play()
         oscProp2.type = document.querySelector("input[name = 'waveform']:checked").value
-        play()
-        play()
+        play2()
+        play2()
     }
 
     let mouseState = false
@@ -75,8 +84,8 @@ window.onload = function(){
             play()
             play()
             changeFreq2()
-            play()
-            play()
+            play2()
+            play2()
         }
     })
 }
